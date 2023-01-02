a23f3f56e4724d129d58e7adef707edec3886074
Aditya Gaharwar <92220550+aditya345-coder@users.noreply.github.com>
2023-01-03 00:36:46 +0530

    Update Digital_Clock.py

--- Digital clock/Digital_Clock.py
+++ Digital clock/Digital_Clock.py
@@ -2,17 +2,18 @@ from time import strftime
 from tkinter import Label, Tk
 
 # Window config for clock
-window = Tk()
-window.title("Digital Clock")
+window = Tk() # to create a main window
+window.title("Digital Clock") # title of the window
 window.geometry('200x100') # size of the window
 window.configure(bg="black") # background color of window
 window.resizable(False, False) # window showld be resizable or not
 instance = Label(window,bg='blue',font=("Times",15,'bold'), relief='flat')
-instance.place(x=55,y=35) 
+instance.place(x=55,y=35) # postion of the clock according to the programmer
 
+# Displying Time
 def time():
     current_time=strftime("%H:%M:%S") # current time
     instance.config(text=current_time) # displying current time in tkinter window
     instance.after(80, time) # for updating time
-time()    
-window.mainloop()
+time() # time function call
+window.mainloop() # used when your application is ready to run
